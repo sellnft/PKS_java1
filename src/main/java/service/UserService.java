6f74f0ac2e847fb491cf4fde9488245441c0c363
Рахманинов Савва <savva2006@bk.ru>
2026-09-15 15:35:58 +0300

    feat: add checkings to UserService.java

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -10,6 +10,13 @@ public class UserService {
 
     public void registerUser(String login, String password, String email, String fio, UserRole role) {
 
+    if (userRepository.checkIfUserExistsByLogin(login)) {
+        throw new IllegalArgumentException("Пользователь с таким логином уже существует!");
+    }
+
+    if (userRepository.checkIfUserExistsByEmail(email)) {
+        throw new IllegalArgumentException("Пользователь с таким email уже существует!");
+    }
 
     }
 }
